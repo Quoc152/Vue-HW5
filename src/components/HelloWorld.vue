09c0045031168875d16118b7ae314cef5ebc956c
Quoc Le <quocle152@gmail.com>
2024-10-05 17:46:18 +0700

    hw5

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,9 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const value = ref(0);
+
+const isEven = computed(() => value.value % 2 === 0)
 
 const decrement = () => {
   value.value--;
@@ -15,18 +17,35 @@ const increment = () => {
 
 <template>
   <div class="greetings">
-    <button @click="decrement">
+    <div>        
+      <label :class="isEven ? 'even' : 'odd'">
+        {{ isEven ? 'Số chẵn' : 'Số lẻ' }}
+      </label>
+    </div>
+
+    <div>
+      <button @click="decrement">
       <font-awesome-icon :icon="['fas', 'minus']" />
     </button>
     <input type="number" v-model="value" />
     <button @click="increment">
       <font-awesome-icon :icon="['fas', 'plus']" />
     </button>
-    
+    </div>
   </div>
 </template>
 
 <style scoped>
+.even {
+  color: green;
+  margin: 0 20px;
+}
+
+.odd {
+  color: yellow;
+  margin: 0 20px;
+}
+
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
@@ -47,6 +66,12 @@ button {
   width: 50px;
   height: 30px;
   margin: 0 10px;
+}
+
+.greetings {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 }
 
 .greetings h1,
